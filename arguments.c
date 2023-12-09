717850d1ca97e46631590995463b4c0f9d9b8554
Makenalyn <lyntonmakena@gmail.com>
2023-12-09 13:12:42 -0800

    prints the name of the arguments supplied to the pg

--- arguments.c
+++ arguments.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char argv[])
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: (0)
+ */
+int main(int argc, char **argv)
 {
-	printf("%d\n", argc);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argument at index[%d] is %s\n", i, argv[i]);
+	}
 	return (0);
 }
